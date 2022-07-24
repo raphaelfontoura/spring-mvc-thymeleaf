a104b1c4481f8ef922534c8f2aa73486b9e2ac86
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-24 14:43:34 -0300

    Refactor filter status. Add th:classappend to links

--- src/main/java/com/rddev/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/rddev/mvc/mudi/controller/HomeController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,13 +33,19 @@ public class HomeController {
     return modelAndView;
   }
 
-  @GetMapping("/aguardando")
-  public String aguardando(Model model) {
+  @GetMapping("/{status}")
+  public String porStatus(@PathVariable("status") String status, Model model) {
 
-    List<Pedido> pedidos = repository.findByStatus(StatusPedido.AGUARDANDO);
+    List<Pedido> pedidos = repository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
 
     model.addAttribute("pedidos", pedidos);
+    model.addAttribute("status", status);
 
     return "home";
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public String onError() {
+    return "redirect:/home";
   }
 }
