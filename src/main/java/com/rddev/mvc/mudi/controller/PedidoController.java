262b43b2f6dead34795f602dcd8dd30f8bb5f735
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-24 13:06:24 -0300

    Add validation to new product

---
+++ src/main/java/com/rddev/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,37 @@
+package com.rddev.mvc.mudi.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rddev.mvc.mudi.dto.RequisicaoNovoPedido;
+import com.rddev.mvc.mudi.model.Pedido;
+import com.rddev.mvc.mudi.repository.PedidoRepository;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+
+  @Autowired
+  private PedidoRepository pedidoRepository;
+
+  @GetMapping("formulario")
+  public String formulario(RequisicaoNovoPedido novoPedido) {
+    return "pedido/formulario";
+  }
+
+  @PostMapping("novo")
+  public String novo(@Valid RequisicaoNovoPedido novoPedido, BindingResult result) {
+    if (result.hasErrors()) return "pedido/formulario";
+
+    Pedido pedido = novoPedido.toPedido();
+    pedidoRepository.save(pedido);
+
+    return "/home";
+  }
+}
---
+++ src/main/java/com/rddev/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,35 @@
+package com.rddev.mvc.mudi.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+import com.rddev.mvc.mudi.model.Pedido;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequisicaoNovoPedido {
+  
+  @NotBlank // NotBlank.requisicaoNovoPedido.nomeProduto= não pode estar em branco.
+  @Min(3)
+  private String nomeProduto;
+  @NotBlank
+  private String urlProduto;
+  @NotBlank
+  private String urlImagem;
+  private String descricao;
+
+  public Pedido toPedido() {
+    Pedido pedido = new Pedido();
+    pedido.setNomeProduto(nomeProduto);
+    pedido.setUrlProduto(urlProduto);
+    pedido.setUrlImagem(urlImagem);
+    pedido.setDescricao(descricao);
+    return pedido;
+  }
+
+}
