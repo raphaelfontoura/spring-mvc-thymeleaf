3cf3d41ab85e932e0f3a059f64c5a027a86ad0fb
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-24 10:52:32 -0300

    Refactor PedidoRepository to interface and extends JpaRepository

--- src/main/java/com/rddev/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/rddev/mvc/mudi/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
   @GetMapping("/home")
   public String home(Model model) {
 
-    List<Pedido> pedidos = repository.recuperaPedidos();
+    List<Pedido> pedidos = repository.findAll();
 
     model.addAttribute("pedidos", pedidos);
 
--- src/main/java/com/rddev/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/rddev/mvc/mudi/repository/PedidoRepository.java
@@ -1,24 +1,11 @@
 package com.rddev.mvc.mudi.repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rddev.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidoRepository {
-
-  @PersistenceContext
-  private EntityManager entityManager;
-
-  public List<Pedido> recuperaPedidos() {
-    Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-    return query.getResultList();
-  }
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
   
 }
