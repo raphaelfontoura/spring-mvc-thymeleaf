7331e6234e9480451251033821b73985358ba262
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-25 15:22:46 -0300

    Refactor WebSecurityConfig to component-based

--- src/main/java/com/rddev/mvc/mudi/config/WebSecurityConfig.java
+++ src/main/java/com/rddev/mvc/mudi/config/WebSecurityConfig.java
@@ -12,24 +12,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig {
 
-  
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .authorizeRequests()
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((authz) -> authz
         .anyRequest().authenticated()
-      .and()
-        .httpBasic();
+      )
+      .httpBasic();
+      
+    return http.build();
   }
 
   @Bean
-  @Override
-  protected UserDetailsService userDetailsService() {
+  public UserDetailsService userDetailsService() {
 
     UserDetails user = 
       User.withDefaultPasswordEncoder()
