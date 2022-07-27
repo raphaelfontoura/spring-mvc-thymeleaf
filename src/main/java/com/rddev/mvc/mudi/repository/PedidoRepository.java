b43cdf1d4ac2bf54647f81c9c5a8863e3d712278
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-27 00:02:01 -0300

    Add cache and enableCaching. Erase hello world controller

--- src/main/java/com/rddev/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/rddev/mvc/mudi/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package com.rddev.mvc.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,6 +17,7 @@ public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
   List<Pedido> findAllByUserUsername(String username);
 
+  @Cacheable("pedidos")
   List<Pedido> findByStatus(StatusPedido entregue, Pageable pageable);
   
 }
